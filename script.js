c22c1c54daa5c7f51399cec4aeca965be87d50b6
pablodezutter <dezutterpablo@gmail.com>
2020-08-06 22:54:09 +0200

    third commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+/*--let colors --*/
+
+
+
+
+document.getElementById("kleurenknop").addEventListener('click', PickColor);
+ 
+
+
+function PickColor(){
+    document.getElementById('red').style.color = 'red'
+
+}
+
+
+
+
+
